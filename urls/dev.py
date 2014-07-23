8a2e1dd45e1233c5d4bac05417371c6b27b8bd70
Hoa Le <hoale@teracy.com>
2014-07-23 08:38:21 +0000

    Namespacing URL names

--- urls/dev.py
+++ urls/dev.py
@@ -14,7 +14,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns += (
-	url(r'^polls/', include('apps.polls.urls')),
+	url(r'^polls/', include('apps.polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
 
