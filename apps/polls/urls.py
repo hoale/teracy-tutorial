9bb9600a6e729f23c4897b93c95c03f4913ea720
Hoa Le <hoale@teracy.com>
2014-07-24 03:23:02 +0000

    Amend URLconf

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 	# ex: /polls/5
 		url(r'^(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
 	# ex: /polls/5/results/
-		url(r'^(?P<poll_id>\d+)/results/$', views.detail, name='results'),
+		url(r'^(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 	# ex: /polls/5/vote/
 		url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 	)
